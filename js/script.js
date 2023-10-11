7473639c5f996221bdce27b4d8573bd1f81937a4
Naili Azizah <63756219+nailiazh@users.noreply.github.com>
2023-10-11 20:48:51 +0700

    first upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// ganti text berdasarkan class
+// const title = document.getElementsByClassName('title')
+// title.item(0).innerHTML = "hello world"
+// console.log(title)
+// function changeTitle() {
+//     title.innerHTML = "hello"
+// }
+
+// ganti text berdasarkan id
+// const title = document.getElementById('title')
+// title.innerHTML = "hello world"
+// console.log(title)
+// function changeTitle() {
+//     title.innerHTML = "hello"
+// }
+
+// mengganti teks ketika klik button
+// const title = document.getElementById('title')
+// const conversionButton = document.getElementById('button-konversi')
+
+// conversionButton.addEventListener('click', changeTitle)
+// function changeTitle() {
+//     title.innerHTML = "Ini Kalkulator"
+// }
+
+//atau
+// conversionButton.addEventListener('click', function(){
+//     title.innerHTML = "Ini Kalkulator"
+// })
+
+// reverse = ditukar form beserta valuenya langsung
