0bee8d227c1dd8b80ed99a02739063cf7eaafa7a
Naili Azizah <63756219+nailiazh@users.noreply.github.com>
2023-10-12 18:25:45 +0700

    rapi js

--- js/script.js
+++ js/script.js
@@ -29,62 +29,33 @@ switchBtn.addEventListener('click', function(){
 })
 // Konversi
 btnKonversi.addEventListener('click',function(){
-    celciusInp = document.getElementById('celcius-input').value
+    celciusInp = document.getElementById('convers-field').value
     hasil = Number(celciusInp * (9/5) + 32);
     document.getElementById('output-field').value = hasil
-    document.getElementById('output-field').value = celciusInp + " * (9/5) + 32 = " + hasil
+    document.getElementById('cara-kalk').value = celciusInp + " * (9/5) + 32 = " + hasil
 })
 
 // Reset
 btnReset.addEventListener('click',function(){
-    document.getElementById('farenheit').value = ""
-    document.getElementById('celcius').value = ""
-    document.getElementById('kalkulasi').value = ""  
+    document.getElementById('output-field').value = ""
+    document.getElementById('convers-field').value = ""
+    document.getElementById('cara-kalk').value = ""  
 })
 
 // Reserve
 btnReverse.addEventListener('click',function(){
-    fahrenheit = document.getElementById('farenheit').value
-    hasil = Number((fahrenheit - 32) * 5/9);
-    document.getElementById('celcius').value = hasil
-    document.getElementById('kalkulasi').value = "( " + fahrenheit + " - 32) * 5/9 = " + hasil
     celciusTeks.innerHTML = "Fahrenheit (F)"
     fahrenheitTeks.innerHTML = "Celcius (C)"
     celciusInput.innerHTML = "Fahrenheit (F)"
     fahrenheitInput.innerHTML = "Celcius (C)"
     celciusTitle.innerHTML = "Fahrenheit (F)"
     fahrenheitTitle.innerHTML = "Celcius (C)"
+    switchbtn.innerHTML = "Fahrenheit > Celcius"
     celciusExp.innerHTML = "Suhu S dalam derajat Celcius (C) sama dengan suhu S dalam derajat Fahrenheit kali 9/5 dikurang 32."
+
+    fahrenheit = document.getElementById('farenheit').value
+    hasil = Number((fahrenheit - 32) * 5/9);
+    document.getElementById('celcius').value = hasil
+    document.getElementById('kalkulasi').value = "( " + fahrenheit + " - 32) * 5/9 = " + hasil
 })
 
-// ganti text berdasarkan class
-// const title = document.getElementsByClassName('title')
-// title.item(0).innerHTML = "hello world"
-// console.log(title)
-// function changeTitle() {
-//     title.innerHTML = "hello"
-// }
-
-// ganti text berdasarkan id
-// const title = document.getElementById('title')
-// title.innerHTML = "hello world"
-// console.log(title)
-// function changeTitle() {
-//     title.innerHTML = "hello"
-// }
-
-// mengganti teks ketika klik button
-// const title = document.getElementById('title')
-// const conversionButton = document.getElementById('button-konversi')
-
-// conversionButton.addEventListener('click', changeTitle)
-// function changeTitle() {
-//     title.innerHTML = "Ini Kalkulator"
-// }
-
-//atau
-// conversionButton.addEventListener('click', function(){
-//     title.innerHTML = "Ini Kalkulator"
-// })
-
-// reverse = ditukar form beserta valuenya langsung
