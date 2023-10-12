d5e052c77493e981a2cd0a4e2399b6a947866cd4
Naili Azizah <63756219+nailiazh@users.noreply.github.com>
2023-10-12 17:52:45 +0700

    btn tukar teks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Switch
 const celciusTeks = document.getElementById('celcius-teks')
 const fahrenheitTeks = document.getElementById('fahrenheit-teks')
 const celciusInput = document.getElementById('celcius-input')
@@ -6,7 +5,17 @@ const fahrenheitInput = document.getElementById('fahrenheit-input')
 const celciusTitle = document.getElementById('celcius-title')
 const fahrenheitTitle = document.getElementById('fahrenheit-title')
 const celciusExp = document.getElementById('celcius-exp')
+const switchbtn = document.getElementById('btn-tukar')
 
+var celcius
+var fahrenheit
+var hasil
+
+const btnKonversi = document.getElementById('button-konversi')
+const btnReset = document.getElementById('button-reset')
+const btnReverse = document.getElementById('button-reverse') 
+
+// Switch
 const switchBtn = document.getElementById('btn-tukar')
 switchBtn.addEventListener('click', function(){
     celciusTeks.innerHTML = "Fahrenheit (F)"
@@ -15,10 +24,38 @@ switchBtn.addEventListener('click', function(){
     fahrenheitInput.innerHTML = "Celcius (C)"
     celciusTitle.innerHTML = "Fahrenheit (F)"
     fahrenheitTitle.innerHTML = "Celcius (C)"
+    switchbtn.innerHTML = "Fahrenheit > Celcius"
     celciusExp.innerHTML = "Suhu S dalam derajat Celcius (C) sama dengan suhu S dalam derajat Fahrenheit kali 9/5 dikurang 32."
 })
 // Konversi
-// Reverse
+btnKonversi.addEventListener('click',function(){
+    celciusInput = document.getElementById('celcius-input').value
+    hasil = Number(celciusInput * (9/5) + 32);
+    document.getElementById('farenheit-input').value = hasil
+    document.getElementById('output-field').value = celciusInput + " * (9/5) + 32 = " + hasil
+})
+
+// Reset
+btnReset.addEventListener('click',function(){
+    document.getElementById('farenheit').value = ""
+    document.getElementById('celcius').value = ""
+    document.getElementById('kalkulasi').value = ""  
+})
+
+// Reserve
+btnReverse.addEventListener('click',function(){
+    fahrenheit = document.getElementById('farenheit').value
+    hasil = Number((fahrenheit - 32) * 5/9);
+    document.getElementById('celcius').value = hasil
+    document.getElementById('kalkulasi').value = "( " + fahrenheit + " - 32) * 5/9 = " + hasil
+    celciusTeks.innerHTML = "Fahrenheit (F)"
+    fahrenheitTeks.innerHTML = "Celcius (C)"
+    celciusInput.innerHTML = "Fahrenheit (F)"
+    fahrenheitInput.innerHTML = "Celcius (C)"
+    celciusTitle.innerHTML = "Fahrenheit (F)"
+    fahrenheitTitle.innerHTML = "Celcius (C)"
+    celciusExp.innerHTML = "Suhu S dalam derajat Celcius (C) sama dengan suhu S dalam derajat Fahrenheit kali 9/5 dikurang 32."
+})
 
 // ganti text berdasarkan class
 // const title = document.getElementsByClassName('title')
